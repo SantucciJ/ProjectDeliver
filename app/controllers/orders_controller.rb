ca285340ad6a857e2338321b5ace7ffc2bf255fc
JulienS <jul.santuc@gmail.com>
2019-03-01 17:26:47 +0400

    orders controller created and updated

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,36 @@
+class OrdersController < ApplicationController
+  before action :set_order, only: [:show]
+
+  def index
+    @orders = Order.all
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    @order.save
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+  end
+
+  private
+
+  def set_orders
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:title, :description, :price)
+  end
+end
