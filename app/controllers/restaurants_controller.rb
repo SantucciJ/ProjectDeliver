146cfd4d6fef5c181c80b1d4a0a198543b647025
JulienS <jul.santuc@gmail.com>
2019-03-01 17:24:54 +0400

    restaurant controller created and updated

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,36 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurants, only: [:show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    @restaurant.save
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+  end
+
+  private
+
+  def set_restaurants
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:title, :description)
+  end
+end
