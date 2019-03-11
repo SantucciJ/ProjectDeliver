82853143cb5c8fb32d6d5bf25cb6677550827630
JulienS <jul.santuc@gmail.com>
2019-03-11 09:31:57 +0400

    removed price to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_052935) do
+ActiveRecord::Schema.define(version: 2019_03_11_053055) do
 
   create_table "menus", force: :cascade do |t|
     t.string "name"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2019_03_11_052935) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "price"
   end
 
 end
