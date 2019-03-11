288be5ed1fcab25f6db7d43233fa3863e1e45cfa
JulienS <jul.santuc@gmail.com>
2019-03-11 09:23:50 +0400

    model menu created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_052115) do
+ActiveRecord::Schema.define(version: 2019_03_11_052301) do
+
+  create_table "menus", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
