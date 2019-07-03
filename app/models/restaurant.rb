3bc173d8c9858cbab80f4804412c1c81771e8e76
JulienS <jul.santuc@gmail.com>
2019-07-03 09:55:23 +0400

    Restaurant
    Restaurant link to Dishes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,3 @@
 class Restaurant < ApplicationRecord
-  #has_many :dishes, dependent: :destroy
+  has_many :dishes, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root 'restaurants#index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants do
     resources :dishes, only: [:index, :show]
